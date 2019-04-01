f389a1f73223335e262945b1369af77ff83ee6b9
mario rodrigueza <mariorodriguezan@gmail.com>
2019-04-01 03:07:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,30 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+    n_song = 0
+  songs.each do |song|
+    n_song += 1
+    puts "#{n_song}." + song
+    end
+end
