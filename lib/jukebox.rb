82b9b566c3fed95e0c87ecedeb6b1ededc1b6da4
mario rodrigueza <mariorodriguezan@gmail.com>
2019-04-01 03:07:45 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,18 +10,3 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-def help
-  puts "I accept the following commands:"
-  puts "- help : displays this help message"
-  puts "- list : displays a list of songs you can play"
-  puts "- play : lets you choose a song to play"
-  puts "- exit : exits this program"
-end
-
-def list(songs)
-    n_song = 0
-  songs.each do |song|
-    n_song += 1
-    puts "#{n_song}." + song
-    end
-end
